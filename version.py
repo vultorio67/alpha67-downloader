033bcd1b10d79a32d42838fd6e2cf8f2960284ec
vultorio67 <68811816+vultorio67@users.noreply.github.com>
2021-09-21 18:51:50 +0200

    update

---
+++ version.py
@@ -0,0 +1,113 @@
+import re
+import json
+import urllib
+import urllib.request
+import time
+import tkinter
+from tkinter import filedialog
+import os
+import os.path
+import shutil
+from colorama import Fore, Back, Style
+from datetime import date, datetime
+from ast import literal_eval
+
+
+def createDirectory(name, parent):
+    path = parent + "/" + name
+    isdir = os.path.isdir(path)
+    if isdir == False:
+        directory = name
+        parent_dir = parent
+        path = os.path.join(parent_dir, directory)
+        os.mkdir(path)
+        print("Directory '% s' created" % directory)
+
+
+def needUpdate():
+    def needUpdateJson():
+        user = os.getlogin()
+
+        createDirectory("alpha67_MP", "C:/Users/" + user)
+
+        createDirectory("mods", "C:/Users/" + user + "/alpha67_MP")
+        data = """{ok: salut}"""
+        response = urllib.request.urlopen("https://api.github.com/repos/vultorio67/desktop-tutorial/releases")
+        data = json.loads(response.read())
+        data = data[0]
+        data = data["tag_name"]
+
+        da = date.today()
+        now = datetime.now()
+
+        try:
+            f = open('C:/Users/' + user + '/alpha67_MP/test.json')
+
+            print("File data is already create.")
+            time.sleep(0.5)
+            print("checking the current version.")
+
+            with open('C:/Users/' + user + '/alpha67_MP/test.json', 'r') as json_file:
+                uInfo = json.load(json_file)
+                uInfo = literal_eval(uInfo)
+                uInfo = uInfo["version"]
+            if uInfo != data:
+                return True
+            else:
+                return False
+            # Do something with the file
+        except IOError:
+            with open('C:/Users/' + user + '/alpha67_MP/test.json', 'w') as outfile:
+                json.dump(str({"time": str(now), "version": None}), outfile)
+            print("File not accessible, starting his creation")
+            return True
+
+
+    def needUpdateJar():
+        ok = ""
+
+        path = r'C:\Users\evanm\AppData\Roaming\.minecraft\mods'
+
+        def existFile():
+            try:
+                open('listMod.txt', 'r')
+                return True
+            except:
+
+                return False
+
+        if existFile() == False:
+            with open('listMod.txt', 'w') as f:
+                f.write('')
+
+        with open('listMod.txt') as f:
+            contents = f.readlines()
+            with open('listMod.txt', 'w') as f:
+                f.write('')
+            # print(contents)
+
+        with open('listMod.txt', 'a') as f:
+            for files in os.listdir(path):
+                if os.path.isfile(os.path.join(path, files)):
+                    # print(files)
+                    ok = ok + files
+                    f.write(files)
+
+        contents = contents[0]
+
+        if contents == ok:
+            return False
+        else:
+            return True
+
+    jar = needUpdateJar()
+    jsonU = needUpdateJson()
+
+    if jar == True:
+        print("jar is modified")
+    if jsonU == True:
+        print("json version not up to date.")
+
+    if jar or jsonU == True:
+        print("need update")
+        return True
